0f2a9fc42ff60a710de088cf431b10b443254990
John Wu <john.wu@mail.mcgill.ca>
2016-12-08 20:47:41 -0500

    Added node server

---
+++ app/routes.js
@@ -0,0 +1,7 @@
+
+module.exports = function (app) {
+    // application -------------------------------------------------------------
+    app.get('*', function (req, res) {
+        res.sendFile(__dirname + '/public/index.html'); 
+    });
+};
